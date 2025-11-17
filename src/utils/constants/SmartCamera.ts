5e90756361b1369005273db61e8b9241ab40f5b5
Dhruv Johari <dhruv.johari@businessnext.com>
2025-11-17 10:00:59 +0530

    Implemented Liveliness Detection

---
+++ src/utils/constants/SmartCamera.ts
@@ -0,0 +1,9 @@
+  // Liveness detection
+ export const challenges = [
+    "Blink your eyes",
+    "Turn your head left",
+    "Turn your head right",
+    "Smile",
+    "Nod your head Yes",
+    "Nod your head No",
+  ];
